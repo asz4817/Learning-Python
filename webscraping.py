c840dd44742fd6427832144911f63649a72fe262
asz4817 <133913519+asz4817@users.noreply.github.com>
2024-06-28 15:30:39 -0500

    countries!

---
+++ webscraping.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+#html_text = requests.get("")
+
+
+
+
+
+
+# html_text = requests.get("http://www.roanokechurchofchrist.org/").text
+# soup = BeautifulSoup(html_text, 'lxml')
+
+# project_href = [i['href'] for i in soup.find_all('a', href=True)]
+# print(project_href)
+
+
+# text = soup.find_all("a")
+# for t in text:
+#     print(t.text.strip())
+# print(text)
+
+
